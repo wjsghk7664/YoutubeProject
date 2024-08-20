a6768788957dbc81a58312eec351011578219bf0
chanhue <chanhue467@gmail.com>
2024-08-20 17:07:41 +0900

    feat. modfiy adapter

--- app/src/main/java/com/example/youtubeproject/presentation/adapter/PopularVideosAdapter.kt
+++ app/src/main/java/com/example/youtubeproject/presentation/adapter/PopularVideosAdapter.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.youtubeproject.R
 import kotlinx.android.parcel.Parcelize
 
