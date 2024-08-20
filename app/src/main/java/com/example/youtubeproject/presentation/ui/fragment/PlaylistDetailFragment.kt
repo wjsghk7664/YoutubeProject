ccc7ac78d91ab9a71699a24f8e1310657f3f338a
Rure00 <asnjkd5193@gmail.com>
2024-08-20 15:41:47 +0900

    feat: video detail item 수정

--- app/src/main/java/com/example/youtubeproject/presentation/ui/fragment/PlaylistDetailFragment.kt
+++ app/src/main/java/com/example/youtubeproject/presentation/ui/fragment/PlaylistDetailFragment.kt
@@ -7,11 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.youtubeproject.R
 import com.example.youtubeproject.data.model.Playlist
 import com.example.youtubeproject.databinding.FragmentPlaylistDetailBinding
+import com.example.youtubeproject.presentation.uistate.PlaylistUiState
 import com.example.youtubeproject.presentation.viewmodel.PlaylistViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class PlaylistDetailFragment : Fragment() {
     private var _binding: FragmentPlaylistDetailBinding? = null
@@ -19,17 +24,7 @@ class PlaylistDetailFragment : Fragment() {
 
     private val viewModel: PlaylistViewModel by activityViewModels()
 
-    //TODO: 초기화 방법 위험할수도?
-    private val playlist by lazy {
-        val playlistId = arguments?.getString(PLAYLIST)!!
-        val userId = requireActivity().intent.getStringExtra("userData")!!
-        viewModel.getPlaylistDetail(userId, playlistId)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var playlist: Playlist
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +32,12 @@ class PlaylistDetailFragment : Fragment() {
     ): View {
         _binding = FragmentPlaylistDetailBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
     }
 
     override fun onDestroyView() {
@@ -54,5 +55,33 @@ class PlaylistDetailFragment : Fragment() {
                     putString(PLAYLIST, playlistId)
                 }
             }
+    }
+
+    private fun initView() {
+        lifecycleScope.launch {
+            viewModel.uiState.collectLatest {
+                when(it) {
+                    is PlaylistUiState.Init -> null
+
+                    is PlaylistUiState.GetPlaylistDetailSuccess -> {
+                        playlist = it.playlist
+                        binding.playListTitleTv.text = getString(R.string.playlist_detail_title_text, playlist.title)
+                    }
+
+                    is PlaylistUiState.Failure ->
+                        Toast.makeText(requireContext(), getString(R.string.playlist_failure_message), Toast.LENGTH_SHORT).show()
+
+                    is PlaylistUiState.Loading -> null
+
+                    else -> null
+                }
+            }
+        }
+
+        val playlistId = arguments?.getString(PLAYLIST)!!
+        val userId = requireActivity().intent.getStringExtra("userData")!!
+        viewModel.getPlaylistDetail(userId, playlistId)
+
+
     }
 }
